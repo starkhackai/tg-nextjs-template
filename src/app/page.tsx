7d60d2a2dcf912daf65732bfcfd08e9dfe56c379
machuwey <matvetron@gmail.com>
2025-01-09 21:43:15 +0100

    Revert "feat: test"
    This reverts commit 7232802e21050389df4e63d6a80b01574d0ce26c.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,11 @@ import dynamic from 'next/dynamic';
 import { Link } from '@/components/Link/Link';
 import { LocaleSwitcher } from '@/components/LocaleSwitcher/LocaleSwitcher';
 import { Page } from '@/components/Page';
-import { ArgentWallet } from '@/components/ArgentWallet/ArgentWallet';
 
-
+const ArgentWallet = dynamic(
+  () => import('@/components/ArgentWallet/ArgentWallet').then(mod => mod.ArgentWallet),
+  { ssr: false }
+);
 
 export default function Home() {
   const t = useTranslations('i18n');
